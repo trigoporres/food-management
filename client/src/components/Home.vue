d1cac7941d50a488746c0de93911ba16fc751349
trigoporres <santiagotrigoporres@gmail.com>
2018-07-09 17:55:14 +0200

    agregada vista de la lista de alimentos

---
+++ client/src/components/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div v-for="food in foods" :key="food.id" class="food">
+      <p>{{food.name}}</p>
+      <p>{{food.temporality}}</p>
+      <p>{{food.origin}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'Home',
+  data () {
+    return {
+      foods: []
+    }
+  },
+  created () {
+    axios.get(`http://localhost:3000/food/list`)
+    .then(response => {
+      response.data.forEach((element) => this.foods.push(element))
+    })
+  },
+}
+</script>
+
+<style scoped>
+.food{
+  border: 1px solid green
+}
+</style>
